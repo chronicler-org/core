772f7e76cd34fca9a41dbf6812b9ee6b1da337bb
Ádrian Gama <adrian.gama.se@gmail.com>
2024-02-29 15:21:27 -0300

    fix: updateCustomerDTO validation fields

--- src/customer/dto/update.go
+++ src/customer/dto/update.go
@@ -5,18 +5,13 @@ import (
 )
 
 type UpdateCustomerDTO struct {
-	CPF       string    `validate:"required,max=11" json:"cpf,omitempty"`
-	Name      string    `validate:"required,max=50" json:"name,omitempty"`
-	Email     string    `validate:"required,max=50" json:"email,omitempty"`
-	Phone     string    `validate:"required,max=11" json:"phone,omitempty"`
-	Job       string    `validate:"required" json:"job,omitempty"`
-	BirthDate time.Time `validate:"required" json:"birth_date,omitempty"`
+	Name      string    `validate:"max=50" json:"name,omitempty"`
+	Email     string    `validate:"max=50" json:"email,omitempty"`
+	Phone     string    `validate:"max=11" json:"phone,omitempty"`
+	Job       string    `json:"job,omitempty"`
+	BirthDate time.Time `json:"birth_date,omitempty"`
 }
 
 func (dto *UpdateCustomerDTO) ValidateEmail() bool {
-	return true
-}
-
-func (dto *UpdateCustomerDTO) ValidateCPF() bool {
 	return true
 }

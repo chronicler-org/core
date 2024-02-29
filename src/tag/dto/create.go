920b3bf5dc2af610f304193a54638f466a45f73b
Carlos Adriano <cadrianogamac@gmail.com>
2024-02-29 18:34:37 -0300

    feat: add tagDTO

---
+++ src/tag/dto/create.go
@@ -0,0 +1,13 @@
+package tagDTO
+
+import "strings"
+
+type CreateTagDTO struct {
+	Title string `validate:"required,max=20" json:"name"`
+	Color string `validate:"required,max=20" json:"color"`
+}
+
+func (dto *CreateTagDTO) ValidateHexColor() bool {
+	color := strings.ToLower(dto.Color)
+	return hexRegex.MatchString(color)
+}

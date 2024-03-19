cb3f80dc8b201eabeb78b6419f21cb0fa780b80b
Jorge Mendonca Boaretto Loch <81532096+Jorgeloch@users.noreply.github.com>
2024-03-19 13:33:39 -0300

    Feat/manager (#18)
    * feat: adding dependencies
    
    * refactor: updating .gitignore
    
    * feat: initializing files for manager's entity
    
    * fix: fixing import path of fiber module in manager controller
    
    * feat: adding docker files
    
    * update: update Dockerfile
    
    * feat: creating the basic routes treatment for managers
    
    * Update README.md
    
    * refactor: refactoring modules architecture
    
    * feat: adding validations to createManagerDTO
    
    * feat: adding error handling feature

--- src/manager/controller/controller.go
+++ src/manager/controller/controller.go
@@ -1,81 +1,97 @@
 package managerController
 
 import (
+	"errors"
+
 	managerDTO "github.com/chronicler-org/core/src/manager/dto"
 	"github.com/chronicler-org/core/src/manager/service"
+	serviceErrors "github.com/chronicler-org/core/src/utils/errors"
 	"github.com/gofiber/fiber/v2"
 )
 
 type ManagerController struct {
-  service *managerService.ManagerService
+	service *managerService.ManagerService
 }
 
-func InitManagerController (s *managerService.ManagerService) *ManagerController {
-  return &ManagerController{
-    service: s,
-  } 
+func InitManagerController(s *managerService.ManagerService) *ManagerController {
+	return &ManagerController{
+		service: s,
+	}
 }
-func (controller *ManagerController) HandleFindAll (c *fiber.Ctx) error {
-  managers, err := controller.service.FindAll()
-  if err != nil {
-    return c.SendStatus(fiber.StatusInternalServerError)
-  }
-  return c.Status(fiber.StatusOK).JSON(managers)
+func (controller *ManagerController) HandleFindAll(c *fiber.Ctx) error {
+	managers, err := controller.service.FindAll()
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+	return c.Status(fiber.StatusOK).JSON(managers)
 }
 
 func (controller *ManagerController) HandleFindByID(c *fiber.Ctx) error {
-  id := c.Params("id")
+	id := c.Params("id")
 
-  manager, err := controller.service.FindByID(id)
-  if err != nil {
-    return c.SendStatus(fiber.StatusInternalServerError)
-  }
+	manager, err := controller.service.FindByID(id)
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
 
-  return c.Status(fiber.StatusOK).JSON(manager)
+	return c.Status(fiber.StatusOK).JSON(manager)
 }
 
 func (controller *ManagerController) HandleCreateManager(c *fiber.Ctx) error {
-  var managerDTO managerDTO.CreateManagerDTO
+	var managerDTO managerDTO.CreateManagerDTO
 
-  err := c.BodyParser(&managerDTO)
-  if err != nil {
-    return c.SendStatus(fiber.StatusBadRequest)
-  }
+	err := c.BodyParser(&managerDTO)
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
 
-  newManagerID, err := controller.service.Create(managerDTO)
-  if err != nil {
-    return c.SendStatus(fiber.StatusInternalServerError)
-  }
+	newManagerID, err := controller.service.Create(managerDTO)
 
-  return c.Status(fiber.StatusOK).JSON(fiber.Map{
-    "location": newManagerID,
-  })
+	if err != nil {
+		target := &serviceErrors.ServiceError{}
+		if errors.As(err, &target) {
+			return c.Status(fiber.ErrBadRequest.Code).JSON(fiber.Map{
+				"message": err.Error(),
+			})
+		}
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"location": newManagerID,
+	})
 }
 
 func (controller *ManagerController) HandleUpdateManager(c *fiber.Ctx) error {
-  var managerDTO managerDTO.UpdateManagerDTO
+	var managerDTO managerDTO.UpdateManagerDTO
 
-  err := c.BodyParser(&managerDTO)
-  if err != nil {
-    return c.SendStatus(fiber.StatusBadRequest)
-  }
+	err := c.BodyParser(&managerDTO)
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
 
-  id := c.Params("id")
+	id := c.Params("id")
 
-  managerUpdated, err := controller.service.Update(id, managerDTO) 
-  if err != nil {
-    return c.SendStatus(fiber.StatusInternalServerError)
-  }
+	managerUpdated, err := controller.service.Update(id, managerDTO)
+	if err != nil {
+		target := &serviceErrors.ServiceError{}
+		if errors.As(err, &target) {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"message": err.Error(),
+			})
+		}
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
 
-  return c.Status(fiber.StatusOK).JSON(managerUpdated)
+	return c.Status(fiber.StatusOK).JSON(managerUpdated)
 }
 
 func (controller *ManagerController) HandleDeleteManager(c *fiber.Ctx) error {
-  id := c.Params("id")
+	id := c.Params("id")
 
-  err := controller.service.Delete(id)
-  if err != nil {
-    return c.SendStatus(fiber.StatusInternalServerError)
-  }
-  return c.SendStatus(fiber.StatusOK)
+	err := controller.service.Delete(id)
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+	return c.SendStatus(fiber.StatusOK)
 }
--- src/manager/dto/createManagerDTO.go
+++ src/manager/dto/createManagerDTO.go
@@ -1,23 +1,19 @@
 package managerDTO
 
 import (
-  "time"
+	"time"
+
+	"github.com/klassmann/cpfcnpj"
 )
 
 type CreateManagerDTO struct {
-  CPF       string    `validate:"required,max=11" json:"cpf"`
-  Name      string    `validate:"required,max=50" json:"name"`
-  Email     string    `validate:"required,max=50" json:"email"` 
-  Password  string    `validate:"required,min=8" json:"password"`
-  BirthDate time.Time `validate:"required" json:"birth_date"`
+	CPF       string    `validate:"required,max=11" json:"cpf"`
+	Name      string    `validate:"required,max=50" json:"name"`
+	Email     string    `validate:"required,max=50" json:"email"`
+	Password  string    `validate:"required,min=8" json:"password"`
+	BirthDate time.Time `validate:"required" json:"birth_date"`
 }
 
-func (dto *CreateManagerDTO) ValidateEmail () bool {
-  return true
+func (dto *CreateManagerDTO) Validate() bool {
+	return cpfcnpj.ValidateCPF(dto.CPF)
 }
-
-func (dto *CreateManagerDTO) ValidateCPF() bool {
-  return true
-}
-
-

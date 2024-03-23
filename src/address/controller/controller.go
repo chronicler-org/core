576e1d13b376dad8dd2c23d0c5c813f8e0601f69
joaovmotas <jvmota.dev@gmail.com>
2024-03-23 13:35:00 -0300

    refactor: Address DTO

--- src/address/controller/controller.go
+++ src/address/controller/controller.go
@@ -1,14 +1,13 @@
 package addressController
 
-import (
-	"errors"
+// import (
+// 	"errors"
 
-	addressDTO "github.com/chronicler-org/core/src/address/dto"
-	addressService "github.com/chronicler-org/core/src/address/service"
-	serviceErrors "github.com/chronicler-org/core/src/utils/error"
-)
+// 	addressDTO "github.com/chronicler-org/core/src/address/dto"
+// 	addressService "github.com/chronicler-org/core/src/address/service"
+// 	serviceErrors "github.com/chronicler-org/core/src/utils/error"
+// )
 
-type AddressController struct {
-	service *addressService.AddressService
-}
-
+// type AddressController struct {
+// 	service *addressService.AddressService
+// }
--- src/address/dto/create.go
+++ src/address/dto/create.go
@@ -1,12 +1,11 @@
 package addressDTO
 
 type CreateAddressDTO struct {
-	StreetName string `validate:"required, max=50" json:"streetName"`
-	Neighborhood  string  `validate:"required, max=30" json:"neighborhood"`
-	Number string `validate:"required, max=20" json:"number"`
-	City string `validate:"required, max=30" json:"city"`
-	Estate string `validate:"required, max=30" json:"estate"`
-	CEP string `validate:"required, max=8" json:"cep"`
-	Complement string `json:"complement"`
+	CEP          string `validate:"required, max=8" json:"cep"`
+	City         string `validate:"required, max=30" json:"city"`
+	Number       string `validate:"required, max=20" json:"number"`
+	Estate       string `validate:"required, max=30" json:"estate"`
+	StreetName   string `validate:"required, max=50" json:"streetName"`
+	Complement   string `json:"complement"`
+	Neighborhood string `validate:"required, max=30" json:"neighborhood"`
 }
-

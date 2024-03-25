a91c9c644703c4b7eedd9c3c102061a9914819db
inacio <inacio@pop-os.localdomain>
2024-03-25 16:43:59 -0300

    refactor: Corrigido model de cliente e Tag

--- src/tag/model/tagModel.go
+++ src/tag/model/tagModel.go
@@ -4,13 +4,11 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"gorm.io/gorm"
 )
 
 type Tag struct {
-	gorm.Model
 	ID        uuid.UUID `gorm:"primarykey"`
-	Name     string    `gorm:"uniqueIndex" json:"name"`
+	Name      string    `gorm:"uniqueIndex" json:"name"`
 	Color     string    `json:"color"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`

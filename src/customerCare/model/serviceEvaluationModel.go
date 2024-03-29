84726a3b31d10aead49effe54689e97309b0637d
inacio <inacio@pop-os.localdomain>
2024-03-29 15:41:14 -0300

    refactor: Add modelo de avaliação

---
+++ src/customerCare/model/serviceEvaluationModel.go
@@ -0,0 +1,21 @@
+package customerCareModel
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+
+	customerModel "github.com/chronicler-org/core/src/customer/model"
+)
+
+type ServiceEvaluation struct {
+	ID             uuid.UUID              `gorm:"primarykey"  json:"id"`
+	Score          uint8                  `gorm:"not null" json:"score"`
+	Description    string                 `gorm:"not null" json:"description"`
+	CustomerCareID uuid.UUID              `gorm:"column:customer_care_id;not null" json:"-"`
+	CustomerCare   CustomerCare           `gorm:"foreignKey:CustomerCareID;not null" json:"customer_care"`
+	CustomerCPF    string                 `gorm:"column:customer_cpf;primaryKey;type:varchar(11);not null" json:"-"`
+	Customer       customerModel.Customer `gorm:"foreignKey:CustomerCPF" json:"customer"`
+	CreatedAt      time.Time              `json:"created_at"`
+	UpdatedAt      time.Time              `json:"updated_at"`
+}

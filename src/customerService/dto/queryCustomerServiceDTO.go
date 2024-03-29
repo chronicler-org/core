5e3c931dc423eabe8bcb1038e69a7dfc127d4ca8
inacio <inacio@pop-os.localdomain>
2024-03-29 11:10:40 -0300

    refactor: Corrigido dto de busca e criação de customer service

--- src/customerService/dto/queryCustomerServiceDTO.go
+++ src/customerService/dto/queryCustomerServiceDTO.go
@@ -5,5 +5,5 @@ import appDto "github.com/chronicler-org/core/src/app/dto"
 type QueryCustomerServiceDTO struct {
 	appDto.PaginationDTO
 	TeamID string `validate:"omitempty,uuid" query:"team_id"`
-	Name   string `validate:"omitempty,uuid" query:"name"`
+	CustomerCPF   string `validate:"omitempty,uuid" query:"customer_cpf"`
 }

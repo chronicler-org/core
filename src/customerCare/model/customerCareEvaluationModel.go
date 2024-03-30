7edab2aa0db62607a466e7d59d1dea728cb8f435
inacio <inacio@pop-os.localdomain>
2024-03-30 09:10:10 -0300

    refactor: Corrigido primarykey de customer care

--- src/customerCare/model/customerCareEvaluationModel.go
+++ src/customerCare/model/customerCareEvaluationModel.go
@@ -13,8 +13,8 @@ type CustomerCareEvaluation struct {
 	Score          uint8                  `gorm:"not null" json:"score"`
 	Description    string                 `gorm:"not null" json:"description"`
 	CustomerCareID uuid.UUID              `gorm:"type:uuid;column:customer_care_id;not null" json:"-"`
-	CustomerCare   CustomerCare           `gorm:"foreignKey:ID;references:CustomerCareID" json:"customer_care"`
-	CustomerCPF    string                 `gorm:"column:customer_cpf;primaryKey;type:varchar(11);not null" json:"-"`
+	CustomerCare   CustomerCare           `gorm:"foreignKey:CustomerCareID" json:"customer_care"`
+	CustomerCPF    string                 `gorm:"column:customer_cpf;type:varchar(11);not null" json:"-"`
 	Customer       customerModel.Customer `gorm:"foreignKey:CustomerCPF" json:"customer"`
 	CreatedAt      time.Time              `json:"created_at"`
 	UpdatedAt      time.Time              `json:"updated_at"`

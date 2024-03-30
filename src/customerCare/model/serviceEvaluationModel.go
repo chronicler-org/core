ef4098f71f01af11f7ff9c38a2831c92f7aa8a92
inacio <inacio@pop-os.localdomain>
2024-03-29 21:38:19 -0300

    refactor: Add repositório de avaliação de atendimento

--- src/customerCare/model/serviceEvaluationModel.go
+++ src/customerCare/model/serviceEvaluationModel.go
@@ -12,8 +12,8 @@ type ServiceEvaluation struct {
 	ID             uuid.UUID              `gorm:"primarykey"  json:"id"`
 	Score          uint8                  `gorm:"not null" json:"score"`
 	Description    string                 `gorm:"not null" json:"description"`
-	CustomerCareID uuid.UUID              `gorm:"column:customer_care_id;not null" json:"-"`
-	CustomerCare   CustomerCare           `gorm:"foreignKey:CustomerCareID;not null" json:"customer_care"`
+	CustomerCareID uuid.UUID              `gorm:"type:uuid;column:customer_care_id;not null" json:"-"`
+	CustomerCare   CustomerCare           `gorm:"foreignKey:ID;references:CustomerCareID" json:"customer_care"`
 	CustomerCPF    string                 `gorm:"column:customer_cpf;primaryKey;type:varchar(11);not null" json:"-"`
 	Customer       customerModel.Customer `gorm:"foreignKey:CustomerCPF" json:"customer"`
 	CreatedAt      time.Time              `json:"created_at"`

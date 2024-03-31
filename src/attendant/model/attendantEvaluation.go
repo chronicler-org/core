e4995310e26ff83c67330ce8a4525136b27ab886
inacio <inacio@pop-os.localdomain>
2024-03-30 22:01:58 -0300

    refactor: Corrigido unicidade no model de avaliação de atendente

--- src/attendant/model/attendantEvaluation.go
+++ src/attendant/model/attendantEvaluation.go
@@ -10,9 +10,9 @@ type AttendantEvaluation struct {
 	ID          uuid.UUID `gorm:"primarykey"  json:"id"`
 	Score       uint8     `gorm:"not null" json:"score"`
 	Description string    `gorm:"not null" json:"description"`
-	AvaluatedID uuid.UUID `gorm:"type:uuid;column:avaluated_id;unique;not null" json:"-"`
+	AvaluatedID uuid.UUID `gorm:"type:uuid;column:avaluated_id;not null" json:"-"`
 	Avaluated   Attendant `gorm:"foreignKey:AvaluatedID" json:"avaluated"`
-	AvaluatorID uuid.UUID `gorm:"type:uuid;column:avaluator_id;unique;not null" json:"-"`
+	AvaluatorID uuid.UUID `gorm:"type:uuid;column:avaluator_id;not null" json:"-"`
 	Avaluator   Attendant `gorm:"foreignKey:AvaluatorID" json:"avaluator"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`

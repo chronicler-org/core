bf84d60d6dcd3ef7f3453994cd2a0a3cdfdf4920
inacio <inacio@pop-os.localdomain>
2024-04-04 10:50:41 -0300

    refactor: Ajuste DTO de venda

--- src/sales/dto/updateSaleDTO.go
+++ src/sales/dto/updateSaleDTO.go
@@ -1,10 +1,5 @@
 package salesDTO
 
-import "github.com/google/uuid"
-
 type UpdateSaleDTO struct {
-	CustomerCareID uuid.UUID `validate:"omitempty,uuid" json:"customer_care,omitempty"`
-	TotalValue     int       `validate:"omitempty" json:"total_value,omitempty"`
-	Status         string    `validate:"omitempty,lte=45" json:"status,omitempty"`
-	PaymentMethod  string    `validate:"omitempty,lte=45" json:"payment_method,omitempty"`
+	Status string `validate:"omitempty,lte=45" json:"status,omitempty"`
 }

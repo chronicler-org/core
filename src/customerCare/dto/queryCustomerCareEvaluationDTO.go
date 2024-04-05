45554a06bd21eb99b3eb76f947d9ee2630690c5d
inacio <inacio@pop-os.localdomain>
2024-04-05 20:53:47 -0300

    refactor: Add filtro no dto que customer care evaluation

--- src/customerCare/dto/queryCustomerCareEvaluationDTO.go
+++ src/customerCare/dto/queryCustomerCareEvaluationDTO.go
@@ -4,6 +4,6 @@ import appDto "github.com/chronicler-org/core/src/app/dto"
 
 type QueryCustomerCareEvaluationDTO struct {
 	appDto.PaginationDTO
-	CustomerCareID string `validate:"omitempty,uuid" query:"customer_care_id"`
-	CustomerCPF    string `validate:"omitempty,number,len=11" query:"customer_cpf"`
+	CustomerCareID string `validate:"omitempty,uuid" query:"customer_care_id" pg:"operation=equal"`
+	CustomerCPF    string `validate:"omitempty,cpf" query:"customer_cpf" pg:"operation=like"`
 }

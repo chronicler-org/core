f4bfc4ac51486aaee5e481abfd0e0270b208bb5d
inacio <inacio@pop-os.localdomain>
2024-04-05 20:55:57 -0300

    refactor: Add filtro no dto que cmanager

--- src/manager/dto/queryManagerDTO.go
+++ src/manager/dto/queryManagerDTO.go
@@ -4,6 +4,7 @@ import appDto "github.com/chronicler-org/core/src/app/dto"
 
 type QueryManagerDTO struct {
 	appDto.PaginationDTO
-	TeamID string `validate:"omitempty,uuid" query:"team_id"`
-	Name   string `validate:"omitempty,uuid" query:"name"`
+	TeamID string `validate:"omitempty,uuid" query:"team_id" pg:"operation=equal"`
+	Name   string `validate:"omitempty" query:"name" pg:"operation=like"`
+	CPF    string `validate:"omitempty,cpf" query:"cpf" pg:"operation=like"`
 }
